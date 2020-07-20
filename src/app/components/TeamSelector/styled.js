eb68c5834604dfbada365a3ec04a076e3aac5c18
Rafael Vieira <rafaellvs322@gmail.com>
2020-07-20 15:13:11 -0300

    FEA: add TeamSelector component

---
+++ src/app/components/TeamSelector/styled.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+import theme from 'app/helpers/theme'
+
+export const Teams = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 3rem 0;
+`
+
+export const Team = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  flex-basis: 25%;
+  padding: 1rem;
+  background: ${({ selected }) => selected && theme.colors.hover};
+
+  img {
+    height: 100px;
+  }
+`
+
+export const Buttons = styled.div`
+  display: flex;
+  justify-content: center;
+
+  button {
+    margin: 0 1rem;
+  }
+`
