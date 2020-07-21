c79ec8dcbdc364da368d5a7b9d0ee4ea37fbb360
Rafael Vieira <rafaellvs322@gmail.com>
2020-07-21 17:55:14 -0300

    FEA: add League component

---
+++ src/app/components/League/styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Groups = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 3rem 0;
+`
+
+export const Group = styled.div`
+  padding: 0 2rem;
+`
